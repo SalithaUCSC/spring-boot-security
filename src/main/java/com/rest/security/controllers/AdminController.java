fd331c7797a215d0aebc49055a35129bd1de6a18
Salitha Chathuranga <salithachathuranga94@gmail.com>
2022-09-21 10:32:34 +0530

    Add cross origins

--- src/main/java/com/rest/security/controllers/AdminController.java
+++ src/main/java/com/rest/security/controllers/AdminController.java
@@ -1,11 +1,13 @@
 package com.rest.security.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4200"})
 @RequestMapping("/api/admin")
 public class AdminController {
 
--- src/main/java/com/rest/security/controllers/AuthController.java
+++ src/main/java/com/rest/security/controllers/AuthController.java
@@ -6,12 +6,10 @@ import com.rest.security.dtos.JwtResponse;
 import com.rest.security.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4200"})
 @RequestMapping("/api/auth")
 public class AuthController {
 
